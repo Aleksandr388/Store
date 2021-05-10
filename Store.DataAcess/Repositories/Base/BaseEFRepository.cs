80c77955cbdf444b5c3910c83930ef31f3213fec
nesheretnuysahsa@gmail.com <nesheretnuysahsa@gmail.com>
2021-05-10 14:26:10 +0300

    create base "Generic" repository and "Genetic" interface

--- Store.DataAcess/Repositories/Base/BaseEFRepository.cs
+++ Store.DataAcess/Repositories/Base/BaseEFRepository.cs
@@ -1,10 +1,67 @@
-﻿using System;
+﻿using Microsoft.EntityFrameworkCore;
+using Store.DataAcess.Repositories.Interfaces;
+using System;
 using System.Collections.Generic;
-using System.Text;
+using System.Linq;
+using System.Linq.Expressions;
 
 namespace Store.DataAcess.Repositories.Base
 {
-    public class BaseEFRepository
+    public class BaseEFRepository<TEntity> : IBaseRepository<TEntity> where TEntity : class
     {
+        private readonly DbContext _ctx;
+        private readonly DbSet<TEntity> _dbSet;
+
+        public BaseEFRepository(DbContext context)
+        {
+            _ctx = context;
+            _dbSet = context.Set<TEntity>();
+        }
+
+        public TEntity Create(TEntity model)
+        {
+            _dbSet.Add(model);
+
+            _ctx.SaveChanges();
+
+            return model;
+        }
+
+        public void Delete(TEntity model)
+        {
+            _dbSet.Attach(model);
+
+            _dbSet.Remove(model);
+
+            _ctx.SaveChanges();
+        }
+
+        public IEnumerable<TEntity> GetAll()
+        {
+            return _dbSet.AsNoTracking().ToList();
+        }
+
+        public IEnumerable<TEntity> GetAll(Expression<Func<TEntity, bool>> predicate)
+        {
+            return _dbSet.AsNoTracking().Where(predicate).ToList();
+        }
+
+        public TEntity GetById(long id)
+        {
+            return _dbSet.Find(id);
+        }
+
+        public TEntity Update(TEntity model)
+        {
+            _ctx.Entry(model).State = EntityState.Modified;
+            _ctx.SaveChanges();
+
+            return model;
+        }
+
+        public void SaveChanges()
+        {
+            _ctx.SaveChanges();
+        }
     }
 }
---
+++ Store.DataAcess/Repositories/Interfaces/IBaseRepository.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq.Expressions;
+
+namespace Store.DataAcess.Repositories.Interfaces
+{
+    public interface IBaseRepository<TEntity> where TEntity : class
+    {
+        TEntity Create(TEntity model);
+        TEntity Update(TEntity model);
+        void Delete(TEntity model);
+        TEntity GetById(long id);
+        IEnumerable<TEntity> GetAll();
+        IEnumerable<TEntity> GetAll(Expression<Func<TEntity, bool>> predicate);
+    }
+}
